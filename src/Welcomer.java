7099eb9d9e2f27fd9be10442823f9b71666fc7b2
ShaloNatalie <natalie.shalo@strathmore.edu>
2025-04-28 11:57:51 +0300

    Adding Challange 1.

---
+++ src/Welcomer.java
@@ -0,0 +1,8 @@
+public class Welcomer{
+    void welcome() {
+        System.out.println("HELLO!");
+    }
+    public static void main (String[] args){
+        (new Welcomer()).welcome();
+    }
+}
