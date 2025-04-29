53b1262dfa4afce2354b4fb6b325fd261ca57da1
ShaloNatalie <natalie.shalo@strathmore.edu>
2025-04-29 13:48:08 +0300

    Adding Challange 5.

---
+++ src/LoopExample.java
@@ -0,0 +1,13 @@
+public class LoopExample {
+
+        public static void main(String[] args) {
+            int count = 1;
+
+            do {
+                System.out.println("Count is: " + count);
+                count++;
+            } while (count <= 5);
+        }
+    }
+
+
