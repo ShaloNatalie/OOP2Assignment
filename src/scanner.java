0f869e3e514cf6f31f0994a12fee4d9a7a97d121
ShaloNatalie <natalie.shalo@strathmore.edu>
2025-04-29 13:23:00 +0300

    Adding Challange 4.

---
+++ src/scanner.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class scanner{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Price:");
+        double price = scanner.nextDouble();
+
+        if (price<10000.00){
+            System.out.println("Affordable");
+        }
+        else {
+            System.out.println("Expensive");
+        }
+        scanner.close();
+    }
+}
