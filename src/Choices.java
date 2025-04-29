d6f8c518748ab9176ddd090da5dc29f7299402d2
ShaloNatalie <natalie.shalo@strathmore.edu>
2025-04-29 13:02:24 +0300

    Adding Challange 3.

---
+++ src/Choices.java
@@ -0,0 +1,13 @@
+public class Choices {
+    public static void main(String[] args) {
+        int temp=15;
+
+        if (temp > 25) {
+            System.out.println("Beach day!");
+        } else if (temp > 15) {
+            System.out.println("Keep warm!");
+        } else {
+            System.out.println("Stay indoors!");
+        }
+    }
+}
